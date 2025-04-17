44e2b4f8f88cddc5cba79fd888421c774ff25c6e
Rajiv Ravindra pathak <70104100+rajivravindrapathak@users.noreply.github.com>
2025-04-17 11:31:14 +0530

    Add files via upload

--- server/server.js
+++ server/server.js
@@ -1,1 +1,14 @@
+const app = require('./app');
+const http = require('http');
+const { setupSocket } = require('./socket');
 
+const server = http.createServer(app);
+
+// Set up Socket.io
+setupSocket(server);
+
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`);
+});
